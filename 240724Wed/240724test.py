906dbcff2e75653881fe1ea929b07d7054371ddb
Arne919 <ghgghg96@naver.com>
2024-07-24 16:01:19 +0900

    Update 240724test.py

--- 240724Wed/240724test.py
+++ 240724Wed/240724test.py
@@ -177,3 +177,14 @@
 
 
 # ws_7_5
+# 아래 클래스를 수정하시오.
+class Shape:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def __str__(self):
+        return f'Shape: width={self.width}, height={self.height}'
+
+shape1 = Shape(5, 3)
+print(shape1)
