9f12abead7fd103641e120b9b2f2a92ec3d4c33b
Arne919 <ghgghg96@naver.com>
2024-07-28 20:16:12 +0900

    Update ws_3_a.py

--- 240717/ws_3_a.py
+++ 240717/ws_3_a.py
@@ -3,10 +3,9 @@ def my_multi(number_1, number_2):
     return number_1 * number_2
 # my_multi(2, 3) 결과 : 6
 # 함수를 수정하고 호출 결과를 result_1 변수에 할당하여 출력하시오.
-number_1 = 2
-number_2 = 3
-result_1 = my_multi(number_1, number_2)
+result_1 = my_multi(2, 3)
 print(result_1)
+
 
 
 # 2
