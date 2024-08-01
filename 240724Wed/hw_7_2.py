86fc04acefdd6c2dcc629eb9f12cba06a2212c43
kanghyekyeong <ghgghg96@naver.com>
2024-08-01 10:30:06 +0900

    8/1

--- 240724Wed/hw_7_2.py
+++
@@ -1,9 +0,0 @@
-# 아래 클래스를 수정하시오.
-class StringRepeater:
-    def repeat_string(self, times, string):
-        for _ in range(times):
-            print(string)
-
-
-repeater1 = StringRepeater()
-repeater1.repeat_string(3, "Hello")
