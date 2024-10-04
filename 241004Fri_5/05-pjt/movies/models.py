e50faa8149d2af1232dacaa75485623c7f61b272
kanghyekyeong <ghgghg96@naver.com>
2024-10-04 17:45:32 +0900

    10/4_5_pjt

---
+++ 241004Fri_5/05-pjt/movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    # image_address = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
