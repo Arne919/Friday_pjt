7c371a4e9f5407b1b90b894222129b365ed15c80
KangHyeKyeong <ghgghg96@naver.com>
2024-11-15 11:56:18 +0900

    241115Fri_9

---
+++ 241115Fri_9/example-pjt/src/components/Common/AppNavbar.vue
@@ -0,0 +1,16 @@
+<script setup>
+// setup: Vue 라이프사이클
+// Setup: 최초 세팅
+// Mount: DOM 이 그려진 후
+// Update: 화면 완성 후 데이터 변경 시
+// UnMount: 다른 컴포넌트로 이동 시
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/cart">Cart</RouterLink>
+  </nav>
+</template>
